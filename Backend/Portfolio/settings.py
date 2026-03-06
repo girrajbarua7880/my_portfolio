98b28f9de69f2678c85e103b96cdae09209a344c
Girraj Barua <girrajbarua7880@gmail.com>
2026-03-06 13:42:32 +0530

    Fix Render 400 error

--- Backend/Portfolio/settings.py
+++ Backend/Portfolio/settings.py
@@ -32,6 +32,9 @@ DEBUG = os.getenv("DEBUG") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com"
+]
 
 
 # Application definition
