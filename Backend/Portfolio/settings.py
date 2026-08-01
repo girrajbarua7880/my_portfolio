774f6ef24a1153dcfe1025c2d0d158558d6e4999
Girraj Barua <GIRRAJBARUA7880@gmail.com>
2026-08-01 12:50:17 +0530

    error fix

--- Backend/Portfolio/settings.py
+++ Backend/Portfolio/settings.py
@@ -205,7 +205,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
